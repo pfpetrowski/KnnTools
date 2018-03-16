8c2b41e82593c884b24e50257e8954b80a2dd953
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-03-15 22:11:44 -0500

    Classification
    Implemented function for Knn classification.

--- knnreg.py
+++ knnreg.py
@@ -5,6 +5,7 @@
 #You are free to use and distribute under the terms of the MIT License.
 
 import numpy as np
+from scipy import stats
 
 def dist(vec1, vec2):
 	assert len(vec1) == len(vec2), "Observations must have the same number of predictors."
@@ -16,6 +17,7 @@ def dist(vec1, vec2):
 
 
 def ladist(matrix, vector):
+	assert matrix.shape[1] == len(vector)
 	distances = np.sum(((matrix - vector)**2), axis = 1)**(1/2)
 	return(distances)
 
@@ -27,20 +29,26 @@ def predict(train, sample, targets, k):
 	prediction = np.mean([targets[i] for i in nearest_neighbors])
 	return prediction
 
+def classify(train, sample, targets, k):
+	assert train.shape[0] == len(targets),"Must have equal numbers of samples and targets."
+	distances = ladist(train, sample)
+	nearest_neighbors = np.argsort(distances)[0:k]
+	classification = stats.mode([targets[i] for i in nearest_neighbors])
+	return classification
 
-'''
-def sumsq(data, targets, k):
-	residuals = []
-	for i, observation in enumerate(data):
-		other = np.delete(data,i,0)
-		other_targets = np.delete(targets, i)
-		distances = [dist(observation, other[j]) for j in range(other.shape[0])]
-		nearest_neighbors = np.argsort(distances)[0:k]
-		yhat = np.mean([other_targets[k] for k in nearest_neighbors])
-		residuals.append(yhat - targets[i])
-	rse = [i**2 for i in residuals]
-	return sum(rse)
-'''
+
+#def sumsq(data, targets, k):
+#	residuals = []
+#	for i, observation in enumerate(data):
+#		other = np.delete(data,i,0)
+#		other_targets = np.delete(targets, i)
+#		distances = [dist(observation, other[j]) for j in range(other.shape[0])]
+#		nearest_neighbors = np.argsort(distances)[0:k]
+#		yhat = np.mean([other_targets[k] for k in nearest_neighbors])
+#		residuals.append(yhat - targets[i])
+#	rse = [i**2 for i in residuals]
+#	return sum(rse)
+
 
 
 
